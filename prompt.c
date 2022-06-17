43b60a42c508d50b5b354491f437eb92518159e1
froschi95 <60326840+froschi95@users.noreply.github.com>
2022-06-17 01:11:55 +0100

    Update prompt

--- prompt.c
+++ prompt.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "shell.h"
 
+/**
+* print_prompt - Displays the prompt "$ "
+* Return: void
+*/
+
 void print_prompt(void)
 {
 	    fprintf(stderr, "$ ");
 }
+
+/**
+* print_prompt1 - Displays the continuation prompt "> "
+* Return: void
+*/
 
 void print_prompt1(void)
 {
